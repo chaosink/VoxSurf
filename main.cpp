1819844112b526abd9e9c029c73bc121f5d146af
chaosink <linhsu0723@qq.com>
2018-11-04 16:40:31 +0800

    coding style

--- main.cpp
+++ main.cpp
@@ -38,189 +38,182 @@ using namespace std;
 // --------------------------------------------------------------
 
 // saves a voxel file (.vox format, can be imported by MagicaVoxel)
-void saveAsVox(const char *fname, const Array3D<bool>& voxs, const v3f &translate, float voxel_size)
-{
-  Array<v3b> palette(256); // RGB palette
-  palette.fill(0);
-  palette[127] = v3b(255, 255, 255);
-  FILE *f;
-  f = fopen(fname, "wb");
-  sl_assert(f != NULL);
-  long sx = voxs.xsize(), sy = voxs.ysize(), sz = voxs.zsize();
-  fwrite(&sx, 4, 1, f);
-  fwrite(&sy, 4, 1, f);
-  fwrite(&sz, 4, 1, f);
+void saveAsVox(const char *fname, const Array3D<bool>& voxs, const v3f &translate, float voxel_size) {
+	Array<v3b> palette(256); // RGB palette
+	palette.fill(0);
+	palette[127] = v3b(255, 255, 255);
+	FILE *f;
+	f = fopen(fname, "wb");
+	sl_assert(f != NULL);
+	long sx = voxs.xsize(), sy = voxs.ysize(), sz = voxs.zsize();
+	fwrite(&sx, 4, 1, f);
+	fwrite(&sy, 4, 1, f);
+	fwrite(&sz, 4, 1, f);
 
-  fwrite(&translate[0], 4, 1, f);
-  fwrite(&translate[1], 4, 1, f);
-  fwrite(&translate[2], 4, 1, f);
+	fwrite(&translate[0], 4, 1, f);
+	fwrite(&translate[1], 4, 1, f);
+	fwrite(&translate[2], 4, 1, f);
 
-  fwrite(&voxel_size, 4, 1, f);
+	fwrite(&voxel_size, 4, 1, f);
 
-  ForIndex(i, sx) {
-    ForIndex(j, sy) {
-      ForIndex(k, sz) {
-        uchar pal = voxs.at(i, j, k) ? 127 : 255;
-        fwrite(&pal, sizeof(uchar), 1, f);
-      }
-    }
-  }
-  fwrite(palette.raw(), sizeof(v3b), 256, f);
-  fclose(f);
+	ForIndex(i, sx) {
+		ForIndex(j, sy) {
+			ForIndex(k, sz) {
+				uchar pal = voxs.at(i, j, k) ? 127 : 255;
+				fwrite(&pal, sizeof(uchar), 1, f);
+			}
+		}
+	}
+	fwrite(palette.raw(), sizeof(v3b), 256, f);
+	fclose(f);
 }
 
 // --------------------------------------------------------------
 
-inline bool isInTriangle(int i, int j, const v3i& p0, const v3i& p1, const v3i& p2, int& _depth)
-{
-  v2i delta_p0 = v2i(i, j) - v2i(p0);
-  v2i delta_p1 = v2i(i, j) - v2i(p1);
-  v2i delta_p2 = v2i(i, j) - v2i(p2);
-  v2i delta10 = v2i(p1) - v2i(p0);
-  v2i delta21 = v2i(p2) - v2i(p1);
-  v2i delta02 = v2i(p0) - v2i(p2);
+inline bool isInTriangle(int i, int j, const v3i& p0, const v3i& p1, const v3i& p2, int& _depth) {
+	v2i delta_p0 = v2i(i, j) - v2i(p0);
+	v2i delta_p1 = v2i(i, j) - v2i(p1);
+	v2i delta_p2 = v2i(i, j) - v2i(p2);
+	v2i delta10 = v2i(p1) - v2i(p0);
+	v2i delta21 = v2i(p2) - v2i(p1);
+	v2i delta02 = v2i(p0) - v2i(p2);
 
-  int64_t c0 = (int64_t)delta_p0[0] * (int64_t)delta10[1] - (int64_t)delta_p0[1] * (int64_t)delta10[0];
-  int64_t c1 = (int64_t)delta_p1[0] * (int64_t)delta21[1] - (int64_t)delta_p1[1] * (int64_t)delta21[0];
-  int64_t c2 = (int64_t)delta_p2[0] * (int64_t)delta02[1] - (int64_t)delta_p2[1] * (int64_t)delta02[0];
-  bool inside = (c0 <= 0 && c1 <= 0 && c2 <= 0) || (c0 >= 0 && c1 >= 0 && c2 >= 0);
+	int64_t c0 = (int64_t)delta_p0[0] * (int64_t)delta10[1] - (int64_t)delta_p0[1] * (int64_t)delta10[0];
+	int64_t c1 = (int64_t)delta_p1[0] * (int64_t)delta21[1] - (int64_t)delta_p1[1] * (int64_t)delta21[0];
+	int64_t c2 = (int64_t)delta_p2[0] * (int64_t)delta02[1] - (int64_t)delta_p2[1] * (int64_t)delta02[0];
+	bool inside = (c0 <= 0 && c1 <= 0 && c2 <= 0) || (c0 >= 0 && c1 >= 0 && c2 >= 0);
 
-  if (inside) {
-    int64_t area = c0 + c1 + c2;
-    int64_t b0 = (c1 << 10) / area;
-    int64_t b1 = (c2 << 10) / area;
-    int64_t b2 = (1 << 10) - b0 - b1;
-    _depth = (int)((b0 * p0[2] + b1 * p1[2] + b2 * p2[2]) >> 10);
-  }
-  return inside;
+	if (inside) {
+		int64_t area = c0 + c1 + c2;
+		int64_t b0 = (c1 << 10) / area;
+		int64_t b1 = (c2 << 10) / area;
+		int64_t b2 = (1 << 10) - b0 - b1;
+		_depth = (int)((b0 * p0[2] + b1 * p1[2] + b2 * p2[2]) >> 10);
+	}
+	return inside;
 }
 
 // --------------------------------------------------------------
 
-class swizzle_xyz
-{
+class swizzle_xyz {
 public:
-  inline v3i forward(const v3i& v)  const { return v; }
-  inline v3i backward(const v3i& v) const { return v; }
+	inline v3i forward(const v3i& v)  const { return v; }
+	inline v3i backward(const v3i& v) const { return v; }
 };
 
-class swizzle_zxy
-{
+class swizzle_zxy {
 public:
-  inline v3i forward(const v3i& v)  const { return v3i(v[2], v[0], v[1]); }
-  inline v3i backward(const v3i& v) const { return v3i(v[1], v[2], v[0]); }
+	inline v3i forward(const v3i& v)  const { return v3i(v[2], v[0], v[1]); }
+	inline v3i backward(const v3i& v) const { return v3i(v[1], v[2], v[0]); }
 };
 
-class swizzle_yzx
-{
+class swizzle_yzx {
 public:
-  inline v3i forward(const v3i& v)  const { return v3i(v[1], v[2], v[0]); }
-  inline v3i backward(const v3i& v) const { return v3i(v[2], v[0], v[1]); }
+	inline v3i forward(const v3i& v)  const { return v3i(v[1], v[2], v[0]); }
+	inline v3i backward(const v3i& v) const { return v3i(v[2], v[0], v[1]); }
 };
 
 // --------------------------------------------------------------
 
 template <class S>
 void rasterize(
-  const v3u&                  tri,
-  const std::vector<v3i>&     pts,
-  Array3D<bool>&             _voxs)
-{
-  const S swizzler;
-  v3i tripts[3] = {
-    swizzler.forward(pts[tri[0]]),
-    swizzler.forward(pts[tri[1]]),
-    swizzler.forward(pts[tri[2]])
-  };
-  // check if triangle is valid
-  v2i delta10 = v2i(tripts[1]) - v2i(tripts[0]);
-  v2i delta21 = v2i(tripts[2]) - v2i(tripts[1]);
-  v2i delta02 = v2i(tripts[0]) - v2i(tripts[2]);
-  if (delta10 == v2i(0)) return;
-  if (delta21 == v2i(0)) return;
-  if (delta02 == v2i(0)) return;
-  if (delta02[0] * delta10[1] - delta02[1] * delta10[0] == 0) return;
-  // proceed
-  AAB<2, int> pixbx;
-  pixbx.addPoint(v2i(tripts[0]) / FP_SCALE);
-  pixbx.addPoint(v2i(tripts[1]) / FP_SCALE);
-  pixbx.addPoint(v2i(tripts[2]) / FP_SCALE);
-  for (int j = pixbx.minCorner()[1]; j <= pixbx.maxCorner()[1]; j++) {
-    for (int i = pixbx.minCorner()[0]; i <= pixbx.maxCorner()[0]; i++) {
-      int depth;
-      if (isInTriangle(
-        (i << FP_POW) + (1 << (FP_POW - 1)), // centered
-        (j << FP_POW) + (1 << (FP_POW - 1)), // centered
-        tripts[0], tripts[1], tripts[2], depth)) {
-        v3i vx = swizzler.backward(v3i(i, j, depth >> FP_POW));
-        // tag the voxel as occupied
-        // NOTE: voxels are likely to be tagged multiple times (e.g. center exactly on edge, overlaps, etc.)
-        _voxs.at(vx[0], vx[1], vx[2]) = true;
-      }
-    }
-  }
+	const v3u&              tri,
+	const std::vector<v3i>& pts,
+	Array3D<bool>&          _voxs
+) {
+	const S swizzler;
+	v3i tripts[3] = {
+		swizzler.forward(pts[tri[0]]),
+		swizzler.forward(pts[tri[1]]),
+		swizzler.forward(pts[tri[2]])
+	};
+	// check if triangle is valid
+	v2i delta10 = v2i(tripts[1]) - v2i(tripts[0]);
+	v2i delta21 = v2i(tripts[2]) - v2i(tripts[1]);
+	v2i delta02 = v2i(tripts[0]) - v2i(tripts[2]);
+	if (delta10 == v2i(0)) return;
+	if (delta21 == v2i(0)) return;
+	if (delta02 == v2i(0)) return;
+	if (delta02[0] * delta10[1] - delta02[1] * delta10[0] == 0) return;
+	// proceed
+	AAB<2, int> pixbx;
+	pixbx.addPoint(v2i(tripts[0]) / FP_SCALE);
+	pixbx.addPoint(v2i(tripts[1]) / FP_SCALE);
+	pixbx.addPoint(v2i(tripts[2]) / FP_SCALE);
+	for (int j = pixbx.minCorner()[1]; j <= pixbx.maxCorner()[1]; j++) {
+		for (int i = pixbx.minCorner()[0]; i <= pixbx.maxCorner()[0]; i++) {
+			int depth;
+			if (isInTriangle(
+				(i << FP_POW) + (1 << (FP_POW - 1)), // centered
+				(j << FP_POW) + (1 << (FP_POW - 1)), // centered
+				tripts[0], tripts[1], tripts[2], depth)) {
+				v3i vx = swizzler.backward(v3i(i, j, depth >> FP_POW));
+				// tag the voxel as occupied
+				// NOTE: voxels are likely to be tagged multiple times (e.g. center exactly on edge, overlaps, etc.)
+				_voxs.at(vx[0], vx[1], vx[2]) = true;
+			}
+		}
+	}
 }
 
 // --------------------------------------------------------------
 
-int main(int argc, char **argv)
-{
-  try {
-    if(argc < 3) {
-      cout << "Usage: VoxSurf resolution model.stl" << endl;
-      return 0;
-    }
-    int resolution_max = atoi(argv[1]);
-    // load triangle mesh
-    TriangleMesh_Ptr mesh(loadTriangleMesh(argv[2]));
-    // produce (fixed fp) integer vertices and triangles
-    std::vector<v3i> pts;
-    std::vector<v3u> tris;
-    {
-      m4x4f boxtrsf = scaleMatrix(BOX_SCALE)
-        * scaleMatrix(v3f(0.95f) / tupleMax(mesh->bbox().extent()))
-        * translationMatrix(-mesh->bbox().minCorner());
-      // transform vertices
-      pts.resize(mesh->numVertices());
-      ForIndex(p, mesh->numVertices()) {
-        v3f pt   = mesh->posAt(p);
-        v3f bxpt = boxtrsf.mulPoint(pt);
-        v3i ipt  = v3i(clamp(round(bxpt), v3f(0.0f), BOX_SCALE - v3f(1.0f)));
-        pts[p]   = ipt;
-      }
-      // prepare triangles
-      tris.reserve(mesh->numTriangles());
-      ForIndex(t, mesh->numTriangles()) {
-        v3u tri = mesh->triangleAt(t);
-        tris.push_back(tri);
-      }
-    }
+int main(int argc, char **argv) {
+	try {
+		if(argc < 3) {
+			cout << "Usage: VoxSurf resolution model.stl" << endl;
+			return 0;
+		}
+		int resolution_max = atoi(argv[1]);
+		// load triangle mesh
+		TriangleMesh_Ptr mesh(loadTriangleMesh(argv[2]));
+		// produce (fixed fp) integer vertices and triangles
+		std::vector<v3i> pts;
+		std::vector<v3u> tris;
+		{
+			m4x4f boxtrsf = scaleMatrix(BOX_SCALE)
+				* scaleMatrix(v3f(0.95f) / tupleMax(mesh->bbox().extent()))
+				* translationMatrix(-mesh->bbox().minCorner());
+			// transform vertices
+			pts.resize(mesh->numVertices());
+			ForIndex(p, mesh->numVertices()) {
+				v3f pt   = mesh->posAt(p);
+				v3f bxpt = boxtrsf.mulPoint(pt);
+				v3i ipt  = v3i(clamp(round(bxpt), v3f(0.0f), BOX_SCALE - v3f(1.0f)));
+				pts[p]   = ipt;
+			}
+			// prepare triangles
+			tris.reserve(mesh->numTriangles());
+			ForIndex(t, mesh->numTriangles()) {
+				v3u tri = mesh->triangleAt(t);
+				tris.push_back(tri);
+			}
+		}
 
-    v3u resolution(mesh->bbox().extent() / tupleMax(mesh->bbox().extent()) * float(resolution_max));
-    // rasterize into voxels
-    Array3D<bool> voxs(resolution);
-    voxs.fill(false);
-    {
-      Timer tm("rasterization");
-      Console::progressTextInit((int)tris.size());
-      ForIndex(t, tris.size()) {
-        Console::progressTextUpdate(t);
-        rasterize<swizzle_xyz>(tris[t], pts, voxs); // xy view
-        rasterize<swizzle_yzx>(tris[t], pts, voxs); // yz view
-        rasterize<swizzle_zxy>(tris[t], pts, voxs); // zx view
-      }
-      Console::progressTextEnd();
-      cerr << endl;
-    }
+		v3u resolution(mesh->bbox().extent() / tupleMax(mesh->bbox().extent()) * float(resolution_max));
+		// rasterize into voxels
+		Array3D<bool> voxs(resolution);
+		voxs.fill(false);
+		{
+			Timer tm("rasterization");
+			Console::progressTextInit((int)tris.size());
+			ForIndex(t, tris.size()) {
+				Console::progressTextUpdate(t);
+				rasterize<swizzle_xyz>(tris[t], pts, voxs); // xy view
+				rasterize<swizzle_yzx>(tris[t], pts, voxs); // yz view
+				rasterize<swizzle_zxy>(tris[t], pts, voxs); // zx view
+			}
+			Console::progressTextEnd();
+			cerr << endl;
+		}
 
-    // save the result
-    stringstream ss;
-    ss << resolution_max;
-    saveAsVox((string(argv[2]) + "_" + ss.str() + ".vox").c_str(), voxs, mesh->bbox().minCorner(), tupleMax(mesh->bbox().extent()) /  0.95f / resolution_max);
-  } catch (Fatal& e) {
-    cerr << "[ERROR] " << e.message() << endl;
-  }
-
+		// save the result
+		stringstream ss;
+		ss << resolution_max;
+		saveAsVox((string(argv[2]) + "_" + ss.str() + ".vox").c_str(), voxs, mesh->bbox().minCorner(), tupleMax(mesh->bbox().extent()) / 0.95f / resolution_max);
+	} catch (Fatal& e) {
+		cerr << "[ERROR] " << e.message() << endl;
+	}
 }
 
 /* -------------------------------------------------------- */
